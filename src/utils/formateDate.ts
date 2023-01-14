b7425832ca4c4156246eb2acc7bf0e3da5376ffe
shakil hossain <shakilhossainp.cse@gmail.com>
2023-01-14 14:05:51 +0600

    Notice item add

--- src/utils/formateDate.ts
+++ src/utils/formateDate.ts
@@ -3,3 +3,6 @@ import dayjs from 'dayjs';
 export const formateDate = (date: Date) => {
    return dayjs(date).format('DD MMMM YYYY');
 };
+export const FormateTime = (date: Date) => {
+   return dayjs(date).format('h:mm:ss a');
+};
--- src/views/Notices/index.tsx
+++ src/views/Notices/index.tsx
@@ -1,5 +1,73 @@
-const Notices: React.FC = () => {
-   return <h1>Notices</h1>;
-};
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { Box, Stack } from '@mui/material';
+import { formateDate, FormateTime } from 'utils/formateDate';
+
+function Notices() {
+   return (
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+         <NoticeItem />
+         <Divider variant='inset' component='li' />
+      </List>
+   );
+}
+
+function NoticeItem() {
+   return (
+      <ListItem alignItems='flex-start'>
+         <ListItemAvatar>
+            <Avatar alt='Cindy Baker' src='/static/images/avatar/3.jpg' />
+         </ListItemAvatar>
+         <ListItemText
+            primary={
+               <Box display='flex' justifyContent='space-between'>
+                  <Typography
+                     sx={{ display: 'inline' }}
+                     component='span'
+                     variant='body2'
+                     color='text.primary'
+                  >
+                     Summer BBQ
+                  </Typography>
+                  <Typography
+                     sx={{ display: 'inline' }}
+                     component='span'
+                     variant='body2'
+                     color='text.secondary'
+                  >
+                     {FormateTime(new Date())}
+                  </Typography>
+               </Box>
+            }
+            secondary={
+               <Stack spacing={1}>
+                  <Typography
+                     sx={{ display: 'inline' }}
+                     component='span'
+                     variant='body2'
+                     color='text.disabled'
+                  >
+                     Do you have Paris recommendations? Have you ever…
+                  </Typography>
+                  <Typography
+                     sx={{ display: 'inline' }}
+                     component='span'
+                     variant='body2'
+                     color='text.secondary'
+                  >
+                     {formateDate(new Date())}
+                  </Typography>
+               </Stack>
+            }
+         />
+      </ListItem>
+   );
+}
 
 export default Notices;
